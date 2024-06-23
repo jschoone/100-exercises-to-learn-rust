cb3c5c40f682eef58dc5084392ac592907a033d7
Jan Schoone <6106846+jschoone@users.noreply.github.com>
2024-06-23 10:17:46 +0200

    fix(05_ticket_v2): typo in 05_nullability
    Signed-off-by: Jan Schoone <6106846+jschoone@users.noreply.github.com>

--- exercises/05_ticket_v2/05_nullability/src/lib.rs
+++ exercises/05_ticket_v2/05_nullability/src/lib.rs
@@ -36,7 +36,7 @@ impl Ticket {
         }
     }
     pub fn assigned_to(&self) -> Option<&String> {
-        if let tatus::InProgress { assigned_to } = &self.status {
+        if let Status::InProgress { assigned_to } = &self.status {
             Some(assigned_to)
         } else {
             None
